24167e841329a7f76d0ea0b00766ca4fb1600975
Hafiz ZeeShan <hafizzeeshan138@gmail.com>
2022-01-05 20:31:10 +0500

    Complete product Design

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useGetAllProductsQuery } from "../../features/productsApi";
+import "../Home/Home.css";
 
 const Home = () => {
   const { data, isLoading, error } = useGetAllProductsQuery();
   return (
-    <div className="home-conatiner">
+    <div className="home-container">
       {isLoading ? (
         <p>Loading...</p>
       ) : error ? (
@@ -12,6 +13,19 @@ const Home = () => {
       ) : (
         <>
           <h2>New Arrival</h2>
+          <div className="products">
+            {data.map((product) => (
+              <div className="product" key={product.id}>
+                <h3>{product.name}</h3>
+                <img src={product.image} alt={product.name} />
+                <div className="details">
+                  <span>{product.desc}</span>
+                  <span>${product.price}</span>
+                </div>
+                <button>Add to cart</button>
+              </div>
+            ))}
+          </div>
         </>
       )}
     </div>
